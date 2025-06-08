df937320482cf8c9033372f6c3b629a986018a80
Ihar-Batura <A17331582@gmail.com>
2025-06-08 11:01:35 +0300

    feat: add breadcrumbItems

--- src/pages/product/Product.tsx
+++ src/pages/product/Product.tsx
@@ -2,7 +2,6 @@ import styles from './Product.module.scss';
 import { useState, useEffect } from 'react';
 import { getProductDataById } from '@shared/api/commerce-tools/getProductDataById';
 import { getTokenFromLS } from '@shared/api/local-storage/getTokenFromLS';
-import { getProductIdFromLS } from '@shared/api/local-storage/getProductIdFromLS';
 import ProductTitle from '@components/layout/product/title/ProductTitle';
 import { ButtonBack } from '@components/ui/buttons/ButtonBack';
 import ProductCard from '@components/layout/product/card/ProductCard';
@@ -10,21 +9,24 @@ import { Slider } from '@components/layout/product/slider/Slider';
 import { ProductData, ProductImage } from '@shared/types/types';
 import Benefits from '@components/layout/product/benefits/Benefits';
 import { openDialog } from '@services/DialogService';
+import { useParams } from 'react-router-dom';
+import { Breadcrumbs } from '@components/ui/breadcrumbs/Breadcrumbs';
+import { catalogCategories } from '@shared/constants/categories';
 
 export const ProductPage = () => {
+  const { productId } = useParams<{ productId: string }>();
   const [productData, setProductData] = useState<ProductData | null>(null);
 
   useEffect(() => {
     const fetchProductData = async () => {
       const token = getTokenFromLS();
-      const id = getProductIdFromLS();
 
-      if (!id || !token) {
+      if (!token || !productId) {
         return;
       }
 
       try {
-        const data = await getProductDataById({ id, token });
+        const data = await getProductDataById({ id: productId, token });
         setProductData(data);
       } catch (error) {
         let message = 'Error get product by ID';
@@ -35,12 +37,12 @@ export const ProductPage = () => {
           message = error;
         }
 
-        openDialog(message);
+        openDialog(message, true);
       }
     };
 
     fetchProductData();
-  }, []);
+  }, [productId]);
 
   if (!productData) {
     return <div className={styles.textLoading}>Loading...</div>;
@@ -48,7 +50,29 @@ export const ProductPage = () => {
 
   const currentPath = productData.masterData.current;
 
-  const name = currentPath.name?.['en-US'] || 'Product name';
+  const name = currentPath.name?.['en-US'] ?? 'Product name';
+  const categoryId = productData.masterData.current.categories?.[0]?.id;
+
+  let breadcrumbItems;
+
+  if (categoryId && catalogCategories.get(categoryId)) {
+    const category = catalogCategories.get(categoryId);
+    breadcrumbItems = [
+      { path: '/', name: 'Home' },
+      { path: '/catalog', name: 'Catalog' },
+      {
+        path: category?.path ?? '/catalog',
+        name: category?.name ?? '',
+      },
+      { path: '', name: name },
+    ];
+  } else {
+    breadcrumbItems = [
+      { path: '/', name: 'Home' },
+      { path: '/catalog', name: 'Catalog' },
+      { path: '', name: name },
+    ];
+  }
 
   const price = currentPath.masterVariant?.prices?.[0]?.value?.centAmount
     ? currentPath.masterVariant.prices[0].value.centAmount / 100
@@ -60,29 +84,30 @@ export const ProductPage = () => {
     : null;
 
   const description =
-    currentPath.description?.['en-US'] || 'No description available';
+    currentPath.description?.['en-US'] ?? 'No description available';
 
   const sliderImages = currentPath.masterVariant?.images?.map(
     (img: ProductImage) => ({
       src: img.url,
       alt: 'Product image',
     })
-  ) || [{ src: 'dyson_icon.svg', alt: 'Default image' }];
+  ) ?? [{ src: 'dyson_icon.svg', alt: 'Default image' }];
 
   const variantData =
     currentPath.variants?.map((variant) => {
-      const imageUrl = variant.images?.[0]?.url || 'dyson_icon.svg';
-      const variantName = variant.key || variant.sku || 'Unnamed variant';
+      const imageUrl = variant.images?.[0]?.url ?? 'dyson_icon.svg';
+      const variantName = variant.key ?? variant.sku ?? 'Unnamed variant';
 
       return {
         iconUrl: imageUrl,
         name: variantName,
       };
-    }) || [];
+    }) ?? [];
 
   return (
     <>
       <ProductTitle name={name} price={discountedPrice ?? price} />
+      <Breadcrumbs items={breadcrumbItems} />
       <div className={styles.container}>
         <ButtonBack />
         <div className={styles.productContainer}>
