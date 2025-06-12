9585f8c3a1f65ee1cb4d018d293c484c17b11ebe
Ihar-Batura <A17331582@gmail.com>
2025-06-12 18:11:54 +0300

    feat: add function Logo

---
+++ src/components/layout/rsschool/Logo.tsx
@@ -0,0 +1,30 @@
+import schoolLogo from '../../../assets/images/logo.png';
+import styles from './Logo.module.scss';
+
+export default function Logo() {
+  return (
+    <div className="rsschool">
+      <p className={styles.project}>
+        This project was developed as the final assignment of the Frontend,
+        JavaScript (Stage 2) course.
+      </p>
+      <p className={styles.collab}>
+        Team roles were assigned in Trello, and communication was handled
+        through frequent Google Meet video calls and ongoing Telegram chats.
+      </p>
+
+      <a
+        className={styles.schoolLogo}
+        href="https://rs.school/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          className={styles.logoImage}
+          src={schoolLogo}
+          alt="Rsschool Logo"
+        />
+      </a>
+    </div>
+  );
+}
