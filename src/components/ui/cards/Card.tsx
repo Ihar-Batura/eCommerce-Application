b9642c9b15567bc772657b0408980a5848a89490
Ihar-Batura <A17331582@gmail.com>
2025-06-13 09:49:32 +0300

    feat: add addToCart

--- src/components/ui/cards/Card.tsx
+++ src/components/ui/cards/Card.tsx
@@ -1,5 +1,9 @@
 import styles from './Card.module.scss';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { getCartIdFromLS } from '@shared/api/local-storage/getCartIdFromLS';
+import { useState } from 'react';
+import { apiCreateNewCart } from '@shared/api/commerce-tools/apiCreateNewCart';
+import { apiAddProductToCart } from '@shared/api/commerce-tools/apiAddProductToCart';
 
 interface Card {
   id: string;
@@ -8,6 +12,7 @@ interface Card {
   price: number;
   discountedPrice?: number | null;
   src: string;
+  isInCart: boolean;
 }
 
 export const Card = ({
@@ -17,53 +22,84 @@ export const Card = ({
   price,
   discountedPrice,
   src,
+  isInCart,
 }: Card) => {
-  const navigate = useNavigate();
+  const [inCart, setInCart] = useState(isInCart);
+  const [loading, setLoading] = useState(false);
+
   const alt = 'Product picture';
-  const productName = name.length < 30 ? name : `${name.slice(0, 30)}...`;
+  const maxProductNameLength = 30;
+  const maxProductDescriptionLength = 75;
+  const productName =
+    name.length < maxProductNameLength
+      ? name
+      : `${name.slice(0, maxProductNameLength)}...`;
+
   const productDescription =
-    description.length < 90 ? description : `${description.slice(0, 90)}...`;
+    description.length < maxProductDescriptionLength
+      ? description
+      : `${description.slice(0, maxProductDescriptionLength)}...`;
 
-  function saveIdAndShowProduct() {
-    navigate(`/product/${id}`);
-  }
+  const addToCart = async () => {
+    setLoading(true);
 
-  function handleKeyDown(e: React.KeyboardEvent<HTMLDivElement>) {
-    if (e.key === 'Enter' || e.key === ' ') {
-      e.preventDefault();
-      saveIdAndShowProduct();
+    const cartId = getCartIdFromLS();
+
+    if (cartId === null) {
+      const createCartData = await apiCreateNewCart();
+      const id = createCartData?.id;
+      if (id) {
+        localStorage.setItem('cartIdDyson', id);
+      }
     }
-  }
+
+    apiAddProductToCart(id);
+
+    setLoading(false);
+    setInCart(true);
+  };
 
   return (
-    <div
-      className={styles.container}
-      onClick={saveIdAndShowProduct}
-      onKeyDown={handleKeyDown}
-      role="button"
-      tabIndex={0}
-    >
-      <img src={src} alt={alt} className={styles.cardImage} />
-      <div className={styles.cardInfo}>
-        <div className={styles.cardInfoWrapper}>
-          <div className={styles.priceContainer}>
-            {discountedPrice && (
-              <span className={styles.discountedPrice}>
-                Price: ${(discountedPrice / 100).toFixed(2)}
+    <div className={styles.container}>
+      <Link
+        to={`/product/${id}`}
+        className={styles.linkContainer}
+        onKeyDown={(e) => {
+          if (e.key === ' ') {
+            e.preventDefault();
+            e.currentTarget.click();
+          }
+        }}
+      >
+        <img src={src} alt={alt} className={styles.cardImage} />
+        <div className={styles.cardInfo}>
+          <div className={styles.cardInfoWrapper}>
+            <div className={styles.priceContainer}>
+              {discountedPrice && (
+                <span className={styles.discountedPrice}>
+                  Price: ${(discountedPrice / 100).toFixed(2)}
+                </span>
+              )}
+              <span
+                className={`${styles.price} ${discountedPrice ? styles.strikethrough : ''}`}
+              >
+                {discountedPrice ? 'Initial price' : 'Price'} $
+                {(price / 100).toFixed(2)}
               </span>
-            )}
-            <span
-              className={`${styles.price} ${discountedPrice ? styles.strikethrough : ''}`}
-            >
-              {discountedPrice ? 'Initial price:' : 'Price:'} $
-              {(price / 100).toFixed(2)}
-            </span>
+            </div>
+            <span className={styles.name}>{productName}</span>
+            <span className={styles.line}></span>
           </div>
-          <span className={styles.name}>{productName}</span>
-          <span className={styles.line}></span>
+          <div className={styles.description}>{productDescription}</div>
         </div>
-        <div className={styles.description}>{productDescription}</div>
-      </div>
+      </Link>
+      <button
+        onClick={addToCart}
+        disabled={inCart || loading}
+        className={styles.button}
+      >
+        {loading ? 'Loading...' : inCart ? 'In Cart' : 'Add to Cart'}
+      </button>
     </div>
   );
 };
